ff27eeefa53d51b01e6cf601bbfac0f213f3706c
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-08-19 22:30:33 +0530

    Create DEEP THOUGHT.py

---
+++ DEEP THOUGHT.py
@@ -0,0 +1,5 @@
+a=input("What is the answer to the great question of life, the Universe, and everything").upper().strip()
+if a in ["42","FORTY-TWO","FORTY TWO"]:
+    print("Yes")
+else:
+    print("No")

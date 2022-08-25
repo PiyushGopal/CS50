4861d4cc8c8b884e9ab9544306af40b2cb3fb982
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-08-26 01:54:43 +0530

    Create Plates.py

---
+++ Plates.py
@@ -0,0 +1,43 @@
+def main():
+    plate = input("Plate: ").strip()
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def end_str(a):
+    if a == " ":
+        return True
+    array = a.split()
+    for each in array:
+        if (each.isnumeric()):
+            continue
+        return False
+    return True
+
+
+def is_valid(s):
+    #check for length and check is first element is number of not
+    if (s[0].isnumeric() and s[1].isnumeric()):
+        return False
+    if (len(s) > 6 or len(s) < 2):
+        return False
+    temp = ''
+    # check for find first number and cheack is a 0 or not
+    for one in range(len(s)):
+        # if each var is a number so we found first number in string
+        if (s[one].isnumeric()):
+            # if first number we found is a number
+            # return false
+            if s[one] == '0':
+                return False
+            # if first number is not 0 so just break
+            tempo = s[one:]
+            break
+    if temp != " ":
+        result = end_str(tempo)
+        if result != True:
+            return False
+    return True
+main()

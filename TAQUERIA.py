e27e082b6d8c9ca6fd307896cadea0acafe22ad6
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-09-01 01:43:22 +0530

    Create TAQUERIA.py

---
+++ TAQUERIA.py
@@ -0,0 +1,24 @@
+menu = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+        }
+a=0
+while True:
+    try:
+        item=input("Item:").title()
+        if item in menu:
+            a+= (menu[item])
+            print(f"${a:.2f}")
+    except (ValueError ,KeyError):
+        pass
+    except (EOFError):
+        break
+
+

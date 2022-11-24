d0679287fa274438d3358d107cf8bacb6915e331
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-11-24 22:23:14 +0530

    Create test_twttr.py

---
+++ test_twttr.py
@@ -0,0 +1,8 @@
+from twttr import shorten
+#piyushgopal
+def test_shorten():
+    assert shorten("testing") == "tstng"
+    assert shorten("this is my tweet!") == "ths s my twt!"
+    assert shorten("UPPERCASE") == "PPRCS"
+    assert shorten("s0meth1ng") == "s0mth1ng"
+    #piyushgopal

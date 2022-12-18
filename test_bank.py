076bbb5452da29307f38f567cd54ca85ca1b4da4
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-12-19 01:53:50 +0530

    Create test_bank.py

---
+++ test_bank.py
@@ -0,0 +1,7 @@
+from bank import value
+#piyushgopal
+def test_value():
+    assert value("Hello") == 0
+    assert value("How are you doing?") == 20
+    assert value("What's happening?") == 100
+    #piyushgopal

536ead524f2d7cfacf9099f2aa06d682452e0fdb
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-12-19 02:22:25 +0530

    Update adieu.py

--- adieu.py
+++ adieu.py
@@ -1,18 +1,21 @@
-x = input("Name: ")
-l = []
-while(x):
-    l.append(x)
-    x = input("Name: ")
-if(len(l)==1):
-    print("Adieu, adieu, to",l[0])
-else:
-    if len(l)==2:
-        print("Adieu, adieu, to",l[0],'and',l[1])
+import sys
+import inflect
+p = inflect.engine()
+
+namesList = []
+#piyushgopal
+while True:
+    try:
+        names = input("Name: ").title()
+        if len(names) < 1:
+            sys.exit()
+        #piyushgopal
+        namesList.append(names)
+        output = p.join(namesList)
+#piyushgopal
+    except EOFError:
+        print('\n')
+        print("Adieu, adieu, to " + output)
+        break
     else:
-        string =l[0]+','+' '
-        for i in l[1:len(l)-1]:
-            if l.index(i)!=len(l)-2:
-                string += i+','+' '
-            else:
-                string += i
-        print("Adieu, adieu, to",string,'and',l[len(l)-1])
+        continue
